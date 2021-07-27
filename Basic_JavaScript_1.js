c4351994059452af8c15667f63c41dd498dd0b9c
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-07-27 11:39:23 -0700

    project

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,5 +1,5 @@
 function My_First_Function() {
 var String = "Kiss me, I'm Irish!";
 var result = string.fontcolor("green");
-document.getElementById("Irish".innerHTML = result;
+document.getElementById("Irish").innerHTML = result;
 }
